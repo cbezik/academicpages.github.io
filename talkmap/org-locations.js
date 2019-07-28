698f77ff3b6742384094e9d34ecd830c4d666424
cbezik <harthan@gmail.com>
2019-07-28 18:14:39 -0500

    Update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,17 @@
 var addressPoints = [
   [
+    "Boston, Massachusetts", 
+    42.3602534, 
+    -71.0582912
+  ], 
+  [
     "Los Angeles, California", 
-    34.0029948, 
-    -118.0675151
+    34.0536909, 
+    -118.2427666
+  ], 
+  [
+    "Pittsburgh, Pennsylvania", 
+    40.4416941, 
+    -79.9900861
   ]
 ];
